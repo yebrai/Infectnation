60404bcd2f22f0f468038f4a28aa11d2c1250744
yebrai <ivansevv@gmail.com>
2022-10-05 18:49:34 +0200

    css fix and updated intruction animations

--- javascript/jetpilot.js
+++ javascript/jetpilot.js
@@ -1,18 +1,15 @@
 class Jetpilot{
-    constructor (fps) {
+    constructor () {
         this.img = new Image()
         this.img.src = "./images/jetpilot.png"
         this.x = canvas.width * 0.2
         this.y = canvasElement.height * 0.1
         this.w = 100
         this.h = 100
-        this.fps = fps
-        this.frames = 0
         this.jetPilotImg = false
     }
 
     drawJetpilot = () => {
-        this.frames++
         if (this.jetPilotImg !== true) {
             return
           } else {
